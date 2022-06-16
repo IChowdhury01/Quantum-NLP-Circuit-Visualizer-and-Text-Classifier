e160a540ae947d4f4d8c8dc47ddba0d7b9100d7d
IChowdhuryDA <Ivan.Chowdhury@digital-alpha.com>
2022-06-16 18:58:03 -0400

    Added string diagram rewriting to quantumCase. Moved sample test sentences to constants file.

--- app/src/main/quantumCase.py
+++ app/src/main/quantumCase.py
@@ -1,24 +1,38 @@
 from lambeq import BobcatParser
+from discopy import grammar
+from lambeq import Rewriter
+import sampleSentences
 
-sentence = 'John walks in the park'
+testSentence = sampleSentences.ADS_ISSUED
 
-# Get a string diagram
+# Convert to string diagram
 parser = BobcatParser(verbose='text')
-diagram = parser.sentence2diagram(sentence)
+diagram = parser.sentence2diagram(testSentence)  # syntax-based, not bag-of-words
+grammar.draw(diagram, title='h', figsize=(14, 3), fontsize=12)
 
-from lambeq import AtomicType, IQPAnsatz
+# Rewrite string diagram, to reduce performance costs / training time
+rewriter = Rewriter(['prepositional_phrase', 'determiner'])  # lower tensor count on prepositions
+prep_reduced_diagram = rewriter(diagram).normal_form()
+prep_reduced_diagram.draw(figsize=(9, 4), fontsize=13)
 
-# Define atomic types
-N = AtomicType.NOUN
-S = AtomicType.SENTENCE
+curry_functor = Rewriter(['curry'])  # reduce number of cups
+curried_diagram = curry_functor(prep_reduced_diagram).normal_form()
+curried_diagram.draw(figsize=(5, 4), fontsize=13)
 
-# Convert string diagram to quantum circuit
-ansatz = IQPAnsatz({N: 1, S: 1}, n_layers=2)
-discopy_circuit = ansatz(diagram)
-#discopy_circuit.draw(figsize=(15,10))
 
-from pytket.circuit.display import render_circuit_jupyter
-
-tket_circuit = discopy_circuit.to_tk()
-
-render_circuit_jupyter(tket_circuit)
+# from lambeq import AtomicType, IQPAnsatz
+#
+# # Define atomic types
+# N = AtomicType.NOUN
+# S = AtomicType.SENTENCE
+#
+# # Convert string diagram to quantum circuit
+# ansatz = IQPAnsatz({N: 1, S: 1}, n_layers=2)
+# discopy_circuit = ansatz(diagram)
+# #discopy_circuit.draw(figsize=(15,10))
+#
+# from pytket.circuit.display import render_circuit_jupyter
+#
+# tket_circuit = discopy_circuit.to_tk()
+#
+# render_circuit_jupyter(tket_circuit)
---
+++ data/README.md
@@ -0,0 +1,1 @@
+Placeholder. Add training/testing data here.
